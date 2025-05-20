0f0f670b91ac5978a59be7f230f120e0763e7f7e
Josue Aguilar <josueaguilarjnai@gmail.com>
2025-05-20 00:03:35 -0600

    first commit v1

--- src/config.py
+++ src/config.py
@@ -3,30 +3,37 @@ import os
 import datetime
 import argparse
 
-from config_class import Config
+from config_class import Config, Graph_config, Sample_config, Model_config, Logistic_regression_config, Random_forest_config, SVC_config
 
 def parameters():
-# -------------------------------------
-#    Construct all the parameters
-# -------------------------------------
-    # File CSV
     config = Config()
-    
     args = parse_args()
     config.file = args.file if args.file else config.file
     config.target_column = args.target if args.target else config.target_column
     config.columns = args.columns if args.columns else config.columns
 
-    config.graph_config = graph_config
-        
     if not args.config:
         raise ValueError("No se ha proporcionado un archivo de configuración. Por favor, proporciona el archivo de configuración JSON.")
-    
-    # Cargar y aplicar configuración desde el JSON
+
     config_dict = load_config(args.config)
     for key, value in config_dict.items():
         if hasattr(config, key):
-            setattr(config, key, value)
+            if key == "graph_config" and isinstance(value, dict):
+                setattr(config, key, Graph_config(**value))
+            elif key == "sample_config" and isinstance(value, dict):
+                setattr(config, key, Sample_config(**value))
+            elif key == "model_config" and isinstance(value, dict):
+                # Aquí está la clave:
+                if "logistic_regression_config" in value:
+                    value["logistic_regression_config"] = Logistic_regression_config(**value["logistic_regression_config"])
+                if "random_forest_config" in value:
+                    value["random_forest_config"] = Random_forest_config(**value["random_forest_config"])
+                if "svc_config" in value:
+                    value["svc_config"] = SVC_config(**value["svc_config"])
+                setattr(config, key, Model_config(**value))
+            else:
+                setattr(config, key, value)
+
     # Generar carpetas para guardar resultados
     graph_config = config.graph_config
 
@@ -37,9 +44,8 @@ def parameters():
 
     config.corr_store_file = f"{default_save_path}{config.corr_store_file}.csv"
     config.results_path = f"{default_save_path}/"
-    
-    return config
 
+    return config
 def parse_args():
     parser = argparse.ArgumentParser(description="Modelo de predicción de hipertensión basado en ENSANUT.")
     parser.add_argument("--file", type=str, help="Ruta del archivo CSV.")
